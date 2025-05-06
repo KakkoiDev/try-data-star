c339d244a7e41793d57972a68622bc80aae54071
Cyril <contact@kakkoi.dev>
2025-05-06 17:54:33 +0900

    serve index.html

--- index.ts
+++ index.ts
@@ -1,16 +1,9 @@
 import { ServerSentEventGenerator } from "./lib/serverSentEventGenerator.js";
-import type { Jsonifiable } from "type-fest";
-
-//   `<html><head><script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v${VERSION}/bundles/datastar.js"></script></head><body><div id="toMerge" data-signals-foo="'World'" data-on-load="@get('/merge')">Hello</div></body></html>`,
 
     const server = Bun.serve({
         port: 3000,
         fetch(req) {
-          return new Response("<h1>Hello World</h1>", {
-            headers: {
-                "Content-Type": "text/html",
-            },
-          });
+          return new Response(Bun.file("./comp/index.html"));
         },
       });
       

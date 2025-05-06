75f26d03a4e7ed5a6b81f597efe179047f630bd6
Cyril <contact@kakkoi.dev>
2025-05-06 18:50:58 +0900

    serve home.html

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import { ServerSentEventGenerator } from "./lib/serverSentEventGenerator.js";
 
-    const server = Bun.serve({
-        port: 3000,
-        fetch(req) {
-          return new Response(Bun.file("./comp/index.html"));
-        },
-      });
-      
-      console.log(`Listening on http://localhost:${server.port} ...`);
+const server = Bun.serve({
+	port: 3000,
+	fetch(req) {
+		const url = new URL(req.url);
+
+		if (url.pathname === "/home") {
+			// Creates a new `ServerSentEventGenerator` instance
+			return ServerSentEventGenerator.stream(req, async (stream) => {
+				// Merges HTML fragments into the DOM.
+				const fileContent = await Bun.file("./comp/home.html").text();
+				stream.mergeFragments(fileContent);
+			});
+		}
+
+		return new Response(Bun.file("./comp/index.html"));
+	},
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);

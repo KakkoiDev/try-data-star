b0b33927296a12e11ba53872006d6d63a829f03e
Cyril <contact@kakkoi.dev>
2025-05-07 21:50:04 +0900

    working navigation

--- comp/navbar.ts
+++ comp/navbar.ts
@@ -1,25 +1,15 @@
 class Navbar extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-	}
-
 	connectedCallback() {
 		this.render();
 	}
 
 	render() {
-		this.shadowRoot.innerHTML = /*html*/ `
-        <ul part="navbar">
-            <li part="nav-item" data-on-click="@get('/home')">home</li>
-            <li part="nav-item" data-on-click="@get('/about')">about</li>
+		this.innerHTML = /*html*/ `
+        <ul class="navbar">
+            <li class="nav-item" data-on-click="@get('/home')">home</li>
+            <li class="nav-item" data-on-click="@get('/about')">about</li>
         </ul>
         `;
-	}
-
-	// Apply external styles using CSS custom properties
-	setStyle(propertyName, value) {
-		this.style.setProperty(`--navbar-${propertyName}`, value);
 	}
 }
 

0521baa476b303ab14eefdaa62863bbabe888b0c
Cyril <contact@kakkoi.dev>
2025-05-10 21:55:07 +0900

    make navbar dynamic

--- components/navbar.ts
+++ components/navbar.ts
@@ -1,13 +1,36 @@
 class Navbar extends HTMLElement {
+	_items = [];
+
+	static get observedAttributes() {
+		return ["items"];
+	}
+
+	attributeChangedCallback(name, _oldValue, newValue) {
+		if (name === "items" && newValue) {
+			try {
+				this._items = JSON.parse(newValue);
+				this.render();
+			} catch (error) {
+				console.error("Invalid items format", error);
+			}
+		}
+	}
+
 	connectedCallback() {
 		this.render();
 	}
 
 	render() {
+		const navItems = this._items
+			.map(
+				(item) =>
+					/*html*/ `<li class="nav-item" data-on-click="@get('${item.path}')">${item.label}</li>`,
+			)
+			.join("");
+
 		this.innerHTML = /*html*/ `
         <ul class="navbar">
-            <li class="nav-item" data-on-click="@get('/home')">home</li>
-            <li class="nav-item" data-on-click="@get('/about')">about</li>
+            ${navItems}
         </ul>
         `;
 	}
